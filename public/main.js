20c9fe5bcba64727016a32d0189599a4a5694f7c
Alejandro Martinez <goatmix1@gmail.com>
2022-07-06 12:10:10 -0400

    fixed

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ const ctx = document.getElementById('canvas').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ['Ardrey Kell High School', 'Providence High School', 'Myers Park High School', 'Phillip O. Berry Academy of Technology', 'South Mecklenburg High School'],
+        labels: ['Ardrey Kell High School', 'Providence High School', 'Myers Park High School', 'Phillip O. Berry Academy of Technology', 'South Mecklenburg High School', 'Meadowcreek High School'],
         datasets: [{
             label: 'School Ratings in Charlotte',
             data: [10, 10, 8, 6, 9],
